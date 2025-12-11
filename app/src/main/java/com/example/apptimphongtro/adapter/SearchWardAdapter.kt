86a15ad0e60a25703e9ed890c106244bdc32ff20
Võ Văn Tài <votai130504@gmail.com>
2025-12-11 14:52:44 +0700

    chore: update code filter city and show ward on fragment search

--- app/src/main/java/com/example/apptimphongtro/adapter/SearchWardAdapter.kt
+++ app/src/main/java/com/example/apptimphongtro/adapter/SearchWardAdapter.kt
@@ -3,7 +3,6 @@ package com.example.apptimphongtro.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -16,10 +15,14 @@ class SearchWardAdapter: ListAdapter<Ward,SearchWardAdapter.SearchWardHolder>(Di
 
     inner class SearchWardHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         private val txtWard= itemView.findViewById<TextView>(R.id.txtWard)
-        val checkbox= itemView.findViewById<MaterialCheckBox>(R.id.checkbox)
-
+        val checkbox: MaterialCheckBox = itemView.findViewById(R.id.checkbox)
         fun bin(ward: Ward){
             txtWard.text=ward.wardName
+
+            val isChecked= ward.isCheck
+            itemView.isSelected= isChecked
+            txtWard.isSelected= isChecked
+            checkbox.isChecked=isChecked
         }
     }
 
@@ -32,13 +35,25 @@ class SearchWardAdapter: ListAdapter<Ward,SearchWardAdapter.SearchWardHolder>(Di
       val item= getItem(position)
         holder.bin(item)
 
-        holder.itemView.isSelected= item.isCheck
-        holder.checkbox.isChecked= item.isCheck
+        holder.checkbox.isClickable= false
+        holder.checkbox.isFocusable = false
 
         holder.itemView.setOnClickListener {
-           item.isCheck= !item.isCheck
-            holder.itemView.isSelected=item.isCheck
-            holder.checkbox.isChecked=item.isCheck
+          toggleItem(position)
         }
+
+    }
+
+    private fun toggleItem(position: Int) {
+        val currentList= currentList.toMutableList()
+        val item= currentList[position]
+
+        item.isCheck= !item.isCheck
+        submitList(currentList){
+            notifyDataSetChanged()
+        }
+    }
+    fun getSelectedWard(): List<Ward>{
+        return currentList.filter { it.isCheck  }
     }
 }
--- app/src/main/java/com/example/apptimphongtro/model/Ward.kt
+++ app/src/main/java/com/example/apptimphongtro/model/Ward.kt
@@ -1,11 +1,13 @@
 package com.example.apptimphongtro.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
-
+import kotlinx.parcelize.Parcelize
+@Parcelize
 data class Ward(
 
     @SerializedName("ward")
     val wardName: String,
     var isCheck: Boolean= false
-)
+): Parcelable
