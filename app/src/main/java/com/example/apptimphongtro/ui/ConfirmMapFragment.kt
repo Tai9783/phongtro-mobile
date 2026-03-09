8ec0848d3abca07f9044c61d0d1405406e9df1a9
Võ Văn Tài <votai130504@gmail.com>
2026-03-09 20:00:09 +0700

    feat(map): implement address confirmation using map and geocoder

--- app/src/main/java/com/example/apptimphongtro/ui/ConfirmMapFragment.kt
+++ app/src/main/java/com/example/apptimphongtro/ui/ConfirmMapFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 
 import com.example.apptimphongtro.databinding.FragmentConfirmMapBinding
+import com.example.apptimphongtro.viewmodel.AddPostViewModel
 import com.example.apptimphongtro.BuildConfig as AppBuildConfig
 
 import com.trackasia.android.Trackasia
@@ -17,6 +19,14 @@ import com.trackasia.android.camera.CameraUpdateFactory
 import com.trackasia.android.geometry.LatLng
 import com.trackasia.android.annotations.Marker
 import com.trackasia.android.annotations.MarkerOptions
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
+import kotlin.math.ln
 
 
 class ConfirmMapFragment : Fragment() {
@@ -26,6 +36,8 @@ class ConfirmMapFragment : Fragment() {
     private lateinit var map: TrackasiaMap
     private var currentLatLng: LatLng? = null
     private var marker: Marker? = null
+    private val client= OkHttpClient()
+    private lateinit var addPostViewModel: AddPostViewModel
 
     private val styleUrl by lazy {
         "https://maps.track-asia.com/styles/v2/streets.json?key=${AppBuildConfig.TRACK_ASIA_KEY}"
@@ -34,7 +46,6 @@ class ConfirmMapFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        // TrackAsia init (bản demo họ dùng getInstance(context) không truyền key) :contentReference[oaicite:1]{index=1}
         Trackasia.getInstance(requireContext())
         _binding = FragmentConfirmMapBinding.inflate(inflater, container, false)
         return binding.root
@@ -52,14 +63,18 @@ class ConfirmMapFragment : Fragment() {
                 setupMap()
             }
         }
+        addPostViewModel= ViewModelProvider(requireActivity())[AddPostViewModel::class.java]
+
     }
 
     private fun setupMap() {
         val lat = arguments?.getString("LAT")?.toDouble() ?: 10.8231
         val lng = arguments?.getString("LNG")?.toDouble() ?: 106.6297
+        val originalAddress = arguments?.getString("ADDRESS") ?: "Đang xác định..."
         currentLatLng = LatLng(lat, lng)
-
+        binding.txtAddress.text = originalAddress
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng!!, 15.0))
+        getAddressFromTrackAsisia(lat,lng)
 
         marker = map.addMarker(
             MarkerOptions()
@@ -70,18 +85,58 @@ class ConfirmMapFragment : Fragment() {
         map.addOnMapClickListener { point ->
             marker?.position = point
             currentLatLng = point
+            getAddressFromTrackAsisia(point.latitude, point.longitude)
             true
         }
 
         binding.btnContinue.setOnClickListener {
-            // currentLatLng?.let { saveToDatabase(it.latitude, it.longitude) }
-            // 1. (Tùy chọn) Gửi dữ liệu về trước khi đóng
-            val locationData= 1
-            findNavController().previousBackStackEntry?.savedStateHandle?.set("key_location", locationData)
-
-            // 2. Lệnh để quay lại màn hình trước đó
+            val lag= currentLatLng?.latitude
+            val lng= currentLatLng?.longitude
+            val address= binding.txtAddress.text.toString()
+            if (lag!=null && lng!=null && address.isNotEmpty() && address !="Đang xác định..."){
+                addPostViewModel.updateStep2Address(address,lag,lng)
+            }
             findNavController().popBackStack()
         }
+    }
+    private fun getAddressFromTrackAsisia(lat: Double, lng: Double){
+        //Xử lý hủy các request đang chạy nhằm để tránh tốn dữ liệu do chạy ngầm và chậm máy
+        //dispatcher: Bộ điều phối nơi quản lý tất cả các cuộc gọi mạng đang chờ hoặc đang chạy
+        //queuedCalls(): Đây là danh sách các yêu cầu đang nằm trong "hàng đợi", chưa kịp gửi đi
+        //runningCalls(): Đây là danh sách các yêu cầu đang thực sự "chạy" trên mạng và đợi phản hồi từ server.
+        client.dispatcher.queuedCalls().forEach { if (it.request().tag() == "GET_ADDRESS") it.cancel() }
+        client.dispatcher.runningCalls().forEach { if (it.request().tag() == "GET_ADDRESS") it.cancel() }
+        val url = "https://maps.track-asia.com/api/v2/geocode/json?latlng=$lat%2C$lng&key=${AppBuildConfig.TRACK_ASIA_KEY}&new_admin=true&size=1"
+
+        val request= Request.Builder()
+            .url(url)
+            .tag("GET_ADDRESS") // đặt tag để tự quản    lý
+            .build()
+        // Bắt đầu gọi API
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                if (e.message == "Canceled") return // Bỏ qua nếu là lỗi do mình chủ động hủy
+                // Xử lý khi mất mạng hoặc lỗi server
+                e.printStackTrace()
+            }
+            override fun onResponse(call: Call, response: Response) {
+               val jsonData= response.body?.string()
+                if(jsonData!=null) {
+                    val jsonObject = JSONObject(jsonData)
+                    val status= jsonObject.optString("status")
+                    if(status=="OK"){
+                        val resultArray= jsonObject.getJSONArray("results")
+                        if(resultArray.length()>0){
+                            val result= resultArray.getJSONObject(0)
+                            val fullAddress= result.optString("formatted_address")
+                            activity?.runOnUiThread {
+                                binding.txtAddress.text=fullAddress
+                            }
+                        }
+                    }
+                }
+            }
+        })
     }
 
     //Quản lý vòng đời của MapView
